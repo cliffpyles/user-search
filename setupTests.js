d12287772d4e66f48f1a9d48953e4100de1fa5d6
Cliff Pyles <cliffpyles@gmail.com>
2019-04-17 06:37:20 -0400

    Add support for Polly in tests

--- setupTests.js
+++ setupTests.js
@@ -1,6 +1,24 @@
+import path from 'path'
 import fetch from 'node-fetch'
+import { Polly } from '@pollyjs/core'
+import { setupPolly } from 'setup-polly-jest'
+import NodeHttpAdapter from '@pollyjs/adapter-node-http'
+import FSPersister from '@pollyjs/persister-fs'
 import { escape } from 'querystring'
 import 'jest-extended'
+
+Polly.register(NodeHttpAdapter)
+Polly.register(FSPersister)
+
+setupPolly({
+  adapters: ['node-http'],
+  persister: 'fs',
+  persisterOptions: {
+    fs: {
+      recordingsDir: path.resolve(__dirname, './__recordings__')
+    }
+  }
+})
 
 global.fetch = fetch
 global.encodeURIComponent = escape
