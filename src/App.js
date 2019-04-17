69ad3c584e619e791357febb9194d3b10a6deb29
Cliff Pyles <cliffpyles@gmail.com>
2019-04-17 09:00:34 -0400

    Add support for styles in Webpack config

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { Router } from '@reach/router'
 import store from './store'
 import UserList from './containers/UserList'
+import './App.css'
 
 const App = ({ store }) => {
   return (
