939130265ea7bb4faa5a62782ad6daf62a2b2aab
Cliff Pyles <cliffpyles@gmail.com>
2019-04-17 09:29:59 -0400

    Add initial state for store

--- src/store/initial-state.js
+++ src/store/initial-state.js
@@ -1,1 +1,11 @@
-export default {}
+export default {
+  users: [],
+  user: null,
+  search: '',
+  searchIsPending: false,
+  searchResults: null,
+  searchResultsPrevious: null,
+  searchResultsNext: null,
+  searchResultsFirst: null,
+  searchResultsLast: null
+}
