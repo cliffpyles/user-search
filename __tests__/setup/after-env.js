ad4da3a84d98487d438e27b0b0d8a45f81e39fb5
Cliff Pyles <cliffpyles@gmail.com>
2019-04-18 15:24:07 -0400

    Update test configuration
    Added react-testing-library to the project. Moved test helpers and
    configuration to the __tests__folder. Added a convenience method
    called render to the global space for the tests.

--- __tests__/setup/after-env.js
+++ __tests__/setup/after-env.js
@@ -5,6 +5,9 @@ import { setupPolly } from 'setup-polly-jest'
 import NodeHttpAdapter from '@pollyjs/adapter-node-http'
 import FSPersister from '@pollyjs/persister-fs'
 import { escape } from 'querystring'
+import { render as rtlRender, fireEvent } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
 import 'jest-extended'
 
 Polly.register(NodeHttpAdapter)
@@ -15,11 +18,26 @@ setupPolly({
   persister: 'fs',
   persisterOptions: {
     fs: {
-      recordingsDir: path.resolve(__dirname, './__recordings__')
+      recordingsDir: path.resolve(__dirname, '../recordings')
     }
   }
 })
 
+function render(component) {
+  const { container, getByText } = rtlRender(component)
+
+  container.click = () => {
+    fireEvent.click(container)
+  }
+
+  container.clickText = text => {
+    fireEvent.click(getByText(text))
+  }
+
+  return container
+}
+
 global.fetch = fetch
 global.encodeURIComponent = escape
 global.String = String
+global.render = render
