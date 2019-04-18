0c1e4f0b4e1a02000c5a1e84737df3d4d9c5f123
Cliff Pyles <cliffpyles@gmail.com>
2019-04-18 13:29:00 -0400

    Replace await with promises in api tests

--- src/store/api/api.test.js
+++ src/store/api/api.test.js
@@ -1,27 +1,28 @@
 import { searchUsers, getUserInfo } from './'
 
-test('can search for users', async () => {
-  const results = await searchUsers({ query: 'ladyleet' })
-  const user = results.items.filter(item => item.login === 'ladyleet')
+test('can search for users', () => {
+  return searchUsers({ query: 'ladyleet' }).then(results => {
+    const user = results.items.filter(item => item.login === 'ladyleet')
 
-  expect(results.total_count).toBeNumber()
-  expect(results.items).toBeArray()
-  expect(user.length).toEqual(1)
+    expect(results.total_count).toBeNumber()
+    expect(results.items).toBeArray()
+    expect(user.length).toEqual(1)
+  })
 })
 
-test('can search for users with paginated result', async () => {
-  const results = await searchUsers({ query: 'Ben', perPage: 5, page: 2 })
-
-  expect(results.items).toBeArray()
-  expect(results.items.length).toEqual(5)
-  expect(results.links.prev).toBeDefined()
-  expect(results.links.next).toBeDefined()
-  expect(results.links.last).toBeDefined()
-  expect(results.links.first).toBeDefined()
+test('can search for users with paginated result', () => {
+  return searchUsers({ query: 'Ben', perPage: 5, page: 2 }).then(results => {
+    expect(results.items).toBeArray()
+    expect(results.items.length).toEqual(5)
+    expect(results.links.prev).toBeDefined()
+    expect(results.links.next).toBeDefined()
+    expect(results.links.last).toBeDefined()
+    expect(results.links.first).toBeDefined()
+  })
 })
 
-test('can get user info', async () => {
-  const results = await getUserInfo('jayphelps')
-
-  expect(results).toMatchSnapshot()
+test('can get user info', () => {
+  return getUserInfo('jayphelps').then(results => {
+    expect(results).toMatchSnapshot()
+  })
 })
